9867d89968a5fe00cfa060654b078954359b19b6
anilkumarjatav18 <160883996+anilkumarjatav18@users.noreply.github.com>
2025-07-05 11:39:44 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const port=4000
 //middleware
 app.use(express.json())
 app.use(cors())
-
+    
 //db connection
 connectDb();
 
@@ -36,4 +36,3 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
     console.log(`app is running on port ${port}`)
 })
-//mongodb+srv://anilkumarjatav188:anilkumar12@cluster0.5znzruy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
